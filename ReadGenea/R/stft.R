de9b7313fe4866452221d6ba95f8f36a279f4baf
Zhou Fang <zhou.zfang@gmail.com>
2012-01-24 14:58:07 +0000

    stft axis fixes

--- ReadGenea/R/stft.R
+++ ReadGenea/R/stft.R
@@ -1,9 +1,10 @@
+#if center = T, remove means
 stft <- function(X, win=min(80,floor(length(X)/10)), 
                  inc=min(24, floor(length(X)/30)), coef=64, 
-		 wtype="hanning.window")
+		 wtype="hanning.window", freq = 100, center = T, plot.it = T )
   {
     numcoef <- 2*coef
-    if (win > numcoef)
+    if (win < numcoef)
       {
 	win <- numcoef
 	cat ("stft: window size adjusted to", win, ".\n")
@@ -14,19 +15,20 @@ stft <- function(X, win=min(80,floor(length(X)/10)),
     wincoef <- eval(parse(text=wtype))(win)
 
     ## create a matrix Z whose columns contain the windowed time-slices
-    z <- matrix (0, numwin + 1, numcoef)
-    y <- z
+    z <- matrix (0, numwin + 1, win)
+    y <- matrix(0, numwin+1, win)
     st <- 1
     for (i in 0:numwin)
       {
-	z[i+1, 1:win] <- X[st:(st+win-1)] * wincoef
-	y[i+1,] <- fft(z[i+1,])
+	z[i+1, 1:win] <- (X[st:(st+win-1)] - mean(X[st:(st+win - 1)])* center) * wincoef
+	y[i+1,] <- fft(z[i+1,] )
 	st <- st + inc
       }
 
-    Y<- list (values = Mod(y[,1:coef]), windowsize=win, increment=inc,
-		  windowtype=wtype)
+    Y<- list (values = Mod(y[,(1):coef]), windowsize=win, increment=inc,
+		  windowtype=wtype, center = center, frequency = freq)
     class(Y) <- "stft"
+    if (plot.it) plot.stft(Y)
     return(Y)
   }
 
@@ -41,8 +43,23 @@ hanning.window = function (n) {
 }
 
 
-plot.stft <- function (x, col = gray (63:0/63), ...)
+plot.stft <- function (x, col = gray (63:0/63), log.it = F, log = "", ...)
   {
-    x <- x$values
-    image( z=x, col=col, ...)
+    xv <- x$values
+if (log.it) xv = log(xv)
+time = (x$windowsize/2 +  x$increment * 0:(nrow(xv) - 1))/(x$freq)
+frequency= x$freq * (1:ncol(xv)  - 1 ) / x$windowsize
+if(log == "x"){
+frequency[1] = frequency[2]^2/frequency[3]
 }
+
+    image( x = time , y = frequency,   z=xv, col=col, log = log,...)
+}
+
+
+plot( stft(rep(1, 1000) + c(sin(1:500/ 10 * 2*pi), rep(0, 500)) + c(rep(0, 300),sin(1:500/ 20 * 2*pi), rep(0, 200)) , freq = 1, plot.it = F), log="x")
+stft(sin(1:1000 / (1 +sqrt(1000:1)) * 2 * pi), freq = 1)
+# stft(rep(1, 1000) + sin(1:1000/ 10 * 2*pi), freq = 1)
+
+
+
