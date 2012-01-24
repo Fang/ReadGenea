a12a5d3595e5dffb65974f07793ac6d5a0c79931
Zhou Fang <zhou.zfang@gmail.com>
2012-01-24 10:33:11 +0000

    Added stft

---
+++ ReadGenea/R/stft.R
@@ -0,0 +1,48 @@
+stft <- function(X, win=min(80,floor(length(X)/10)), 
+                 inc=min(24, floor(length(X)/30)), coef=64, 
+		 wtype="hanning.window")
+  {
+    numcoef <- 2*coef
+    if (win > numcoef)
+      {
+	win <- numcoef
+	cat ("stft: window size adjusted to", win, ".\n")
+      }
+    numwin <- trunc ((length(X) - win) / inc)
+
+    ## compute the windows coefficients
+    wincoef <- eval(parse(text=wtype))(win)
+
+    ## create a matrix Z whose columns contain the windowed time-slices
+    z <- matrix (0, numwin + 1, numcoef)
+    y <- z
+    st <- 1
+    for (i in 0:numwin)
+      {
+	z[i+1, 1:win] <- X[st:(st+win-1)] * wincoef
+	y[i+1,] <- fft(z[i+1,])
+	st <- st + inc
+      }
+
+    Y<- list (values = Mod(y[,1:coef]), windowsize=win, increment=inc,
+		  windowtype=wtype)
+    class(Y) <- "stft"
+    return(Y)
+  }
+
+hanning.window = function (n) {
+    if (n == 1) 
+        c <- 1
+    else {
+        n <- n - 1
+        c <- 0.5 - 0.5 * cos(2 * pi * (0:n)/n)
+    }
+    return(c)
+}
+
+
+plot.stft <- function (x, col = gray (63:0/63), ...)
+  {
+    x <- x$values
+    image( z=x, col=col, ...)
+}
