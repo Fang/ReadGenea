b8905c88e67bb4eff993dc7e4396f2b544de7fc6
Zhou Fang <zhou.zfang@gmail.com>
2012-02-17 16:23:10 +0000

    yin f0 finding

---
+++ ReadGenea/R/yin.R
@@ -0,0 +1,28 @@
+#fundamental frequency finding by YIN
+
+#finds the first minimum of a vector under a certain threshold, or the global min
+peakfind.yin = function(t, thresh=0.1){
+ind = c((t < thresh),F)
+if (sum(ind, na.rm= T) > 0){
+beg = match(1,ind)
+end = beg+ match(0, tail(ind, -beg))
+
+return( beg +which.min(t[beg:end]))
+
+} else {
+
+return(which.min(c(Inf,t))-1 )
+}
+}
+
+
+diffobj = sapply(1:300, function(t) (tmp - shift(tmp, 300,fill = "edge"))^2)
+
+diffmed = apply(diffobj, 2, function(t) runmed(t,300))
+
+
+YIN = apply(diffmed, 2, function(t) t / (cumsum(t)/ 1:length(t)))
+
+plot(runmed(apply(YIN,1 , function(t) peakfind.yin(t, 0.6)), 201), type="l")
+
+plot(runmed(apply(YIN,1 , function(t) peakfind.yin(t, 0.6)), 1001), type="l")
