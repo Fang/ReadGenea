1f68cfeecc6107f6b6540c8d66cb935f145d8f3e
Zhou Fang <zhou.zfang@gmail.com>
2012-03-07 16:49:42 +0000

    build for both of zhou's pcs

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
-C:\R\R-2.14.1\bin\x64\Rcmd.exe build ReadGenea
+IF EXIST C:\R\R-2.14.1\bin\x64\Rcmd.exe C:\R\R-2.14.1\bin\x64\Rcmd.exe build ReadGenea
+IF EXIST C:\R2141\bin\x64\Rcmd.exe C:\R2141\bin\x64\Rcmd.exe build ReadGenea
 pause
-C:\R\R-2.14.1\bin\x64\Rcmd.exe INSTALL ReadGenea
+IF EXIST C:\R\R-2.14.1\bin\x64\Rcmd.exe C:\R\R-2.14.1\bin\x64\Rcmd.exe INSTALL ReadGenea
+IF EXIST C:\R2141\bin\x64\Rcmd.exe C:\R2141\bin\x64\Rcmd.exe INSTALL ReadGenea
 pause
-C:\R\R-2.14.1\bin\x64\Rcmd.exe INSTALL --build ReadGenea
+IF EXIST C:\R\R-2.14.1\bin\x64\Rcmd.exe C:\R\R-2.14.1\bin\x64\Rcmd.exe INSTALL --build ReadGenea
+IF EXIST C:\R2141\bin\x64\Rcmd.exe C:\R2141\bin\x64\Rcmd.exe INSTALL --build ReadGenea
 pause
