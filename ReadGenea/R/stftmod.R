23ebe834d6bec9c84403bc1dc15a2ca05eae195d
Zhou Fang <zhou.zfang@gmail.com>
2012-05-16 16:43:18 +0100

    fix progbar issue, quiet setting for stft

--- ReadGenea/R/stftmod.R
+++ ReadGenea/R/stftmod.R
@@ -54,7 +54,7 @@ obj1
 
 stft <- function(X, win=10, 
                  inc=  win/2, coef=Inf, 
-		 wtype="hanning.window", freq , center = T, plot.it = F, calc.null = T , pvalues = F, start.time = NULL, reassign = T )
+		 wtype="hanning.window", freq , center = T, plot.it = F, calc.null = T , pvalues = F, start.time = NULL, reassign = T , quiet = F)
   {
 call = match.call()
 if (length(dim(X)) ==2) {
@@ -77,7 +77,7 @@ Xdel = shift(X, c(1,0), fill = "edge")
     if (win < numcoef)
       {
 	win <- numcoef
-	cat ("stft: window size adjusted to", win, ".\n")
+if (!quiet)	cat ("stft: window size adjusted to", win, ".\n")
       }
     numwin <- trunc ((length(X) - win) / inc)
 
@@ -90,7 +90,7 @@ pval = rep(0, numwin+1)
     y <- matrix(0, numwin+1, win)
     ydel <- matrix(0, numwin+1, win)
     st <- 1
-pb <- txtProgressBar(min = 0, max = 100,style=1)
+if (!quiet) pb <- txtProgressBar(min = 0, max = 100,style=1)
     for (i in 0:numwin)
       {
 	z[i+1, 1:win] <- (X[st:(st+win-1)] - mean(X[st:(st+win - 1)])* center) * wincoef
@@ -107,7 +107,7 @@ pval[i+1] = wilcox.test( (Mod(y[ i+1,])^2 - mean(Mod(y[ i+1,])^2))^2, (temp - me
 }
 	st <- st + inc
 
-setTxtProgressBar(pb, 90* i/(numwin ))
+if (!quiet) setTxtProgressBar(pb, 90* i/(numwin ))
       }
 if (reassign){
 yfreqdel = cbind(y[, win],y[, 2: win - 1])# t(apply(y, 1, function(t) shift(t, 1, fill = "loop")))
@@ -125,11 +125,11 @@ null.logsd = NULL
 if (calc.null){
 tmpdat = stft(sample(X), win = win0, 
                  inc= inc0, coef=coef0, 
-		 wtype=wtype, freq = freq, center = T, plot.it = F, calc.null = F )
+		 wtype=wtype, freq = freq, center = T, plot.it = F, calc.null = F , quiet= T)
 null.logmean = log(sqrt(mean((tmpdat$values)^2)))
 #null.logsd = sd(tmpdat$values))
 }
-setTxtProgressBar(pb, 100)
+if (!quiet)setTxtProgressBar(pb, 100)
 if (is.null(start.time)){
     Y<- list (values = cbind(Mod(y[,1]) ,2*Mod(y[,(2):coef])), windowsize=win, increment=inc,
 		  windowtype=wtype, center = center, sampling.frequency = freq, null.logmean = null.logmean, null.logsd = null.logsd, principals = (freq * (1:coef  - 1 ) / win)[apply( Mod(y[,(1):coef]),1, which.max)], frequency = (freq * (1:coef  - 1 ) / win), times =  (win/2 +  inc * 0:(nrow(y) - 1))/(freq), p.values = pval, LGD = yfreqdel[,1:coef], CIF = ydel[,1:coef] )
@@ -138,7 +138,7 @@ times = (start.time  + (win/2 +  inc * 0:(nrow(y) - 1))/freq)
    Y<- list (values = cbind(Mod(y[,1]) ,2*Mod(y[,(2):coef])), windowsize=win, increment=inc,
 		  windowtype=wtype, center = center, sampling.frequency = freq, null.logmean = null.logmean, null.logsd = null.logsd, principals = (freq * (1:coef  - 1 ) / win)[apply( Mod(y[,(1):coef]),1, which.max)], frequency = (freq * (1:coef  - 1 ) / win), times = times, p.values = pval, LGD = yfreqdel[,1:coef], CIF = ydel[,1:coef]  )
 }
-close(pb)
+if (!quiet)close(pb)
 Y$call = call
     class(Y) <- "stft"
     if (plot.it) plot.stft(Y)
