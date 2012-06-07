6d861f03fac28c363b9bdb72917579356a9d0fd9
Zhou Fang <zhou.zfang@gmail.com>
2012-06-07 17:00:21 +0100

    more docs

--- ReadGenea.Rcheck/ReadGenea-Ex.R
+++ ReadGenea.Rcheck/ReadGenea-Ex.R
@@ -101,6 +101,73 @@ tmp = epoch.quantile(dat, quantiles= c(0.1, 0.25, 0.5, 0.75, 0.9)); matplot(tmp,
 
 graphics::par(get("par.postscript", pos = 'CheckExEnv'))
 cleanEx()
+nameEx("get.intervals")
+### * get.intervals
+
+flush(stderr()); flush(stdout())
+
+### Name: get.intervals
+### Title: Methods for processing and summarising AccData.
+### Aliases: AccData get.intervals [.AccData $.AccData print.AccData
+###   plot.AccData
+### Keywords: manip
+
+### ** Examples
+
+
+binfile  = system.file("binfile/TESTfile.bin", package = "ReadGenea")[1]
+
+#Read in the entire file, calibrated
+procfile<-read.bin(binfile)
+print(procfile)
+procfile$data.out[1:5,]
+
+#Uncalibrated, mmap off
+procfile2<-read.bin(binfile, calibrate = FALSE)
+procfile2$data.out[1:5,]
+
+#Read in again, reusing already computed mmap pagerefs
+procfile3<-read.bin(binfile, pagerefs = procfile2$pagerefs )
+
+#Downsample by a factor of 10
+procfilelo<-read.bin(binfile, downsample = 10)
+print(procfilelo)
+object.size(procfilelo) / object.size(procfile)
+
+#Read in a 1 minute interval
+procfileshort <- read.bin(binfile, start = "16:50", end = "16:51")
+print(procfileshort)
+
+##NOT RUN: Read, and save as a R workspace
+#read.bin(binfile, outfile="tmp.Rdata")
+#print(load("tmp.Rdata"))
+#print(processedfile)
+
+
+
+
+
+cleanEx()
+nameEx("hanning.window")
+### * hanning.window
+
+flush(stderr()); flush(stdout())
+
+### Name: hanning.window
+### Title: Computes the Coefficients of a Hanning or Uniform Window.
+### Aliases: hanning.window uniform.window
+### Keywords: ts
+
+### ** Examples
+hanning.window(10)
+
+x<-rnorm(500)
+y<-stft(x, wtype="hanning.window")
+plot(y)
+
+
+
+cleanEx()
 nameEx("header.info")
 ### * header.info
 
@@ -198,6 +265,42 @@ print(procfileshort)
 #print(load("tmp.Rdata"))
 #print(processedfile)
 
+
+
+
+
+cleanEx()
+nameEx("stft")
+### * stft
+
+flush(stderr()); flush(stdout())
+
+### Name: stft
+### Title: Computes Short Time Fourier Transforms
+### Aliases: stft print.stft plot.stft
+### Keywords: manip
+
+### ** Examples
+
+
+t1 = parse.time("2012-06-21 13:04:01"); print(t1)
+parse.time("21/06/12 13:04:01") #gives the same result
+
+parse.time(c("19/07/70", "20/07/70"), format = "days")
+#results here will depend on your locale
+parse.time(c("19/07/70", "20/07/70"), format = "POSIX", tzone = -4)
+
+#one is the same day, one can only find a match the next day
+parse.time("13:05", start = t1) - t1
+parse.time("13:00", start = t1) - t1
+#asking to wait 1 midnight means both times are considered as 
+#times on the same, full day of data
+parse.time(c("1 13:05", "1 13:00"), start = t1) - t1
+#2012-06-21 is a Thursday, so this is equivalent
+parse.time(c("Fri 13:05", "Fri 13:00"), start = t1) - t1
+#Longer form days of the week are also understood. Note that 
+#the first day does not get matched.
+parse.time(c("Thursday 13:05", "Thursday 13:00"), start = t1) - t1
 
 
 
