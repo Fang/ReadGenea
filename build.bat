b8643fe259eb673c3937123834f34c46f4228031
Zhou Fang <zhou.zfang@gmail.com>
2012-06-19 11:13:30 +0100

    rename package! Slight doc modification

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
-IF EXIST C:\R\R-2.14.1\bin\x64\Rcmd.exe C:\R\R-2.14.1\bin\x64\Rcmd.exe build ReadGenea
-IF EXIST C:\R2141\bin\x64\Rcmd.exe C:\R2141\bin\x64\Rcmd.exe build ReadGenea
+IF EXIST C:\R\R-2.14.1\bin\x64\Rcmd.exe C:\R\R-2.14.1\bin\x64\Rcmd.exe build GENEAread
+IF EXIST C:\R2141\bin\x64\Rcmd.exe C:\R2141\bin\x64\Rcmd.exe build GENEAread
 pause
-IF EXIST C:\R\R-2.14.1\bin\x64\Rcmd.exe C:\R\R-2.14.1\bin\x64\Rcmd.exe INSTALL ReadGenea
-IF EXIST C:\R2141\bin\x64\Rcmd.exe C:\R2141\bin\x64\Rcmd.exe INSTALL ReadGenea
+IF EXIST C:\R\R-2.14.1\bin\x64\Rcmd.exe C:\R\R-2.14.1\bin\x64\Rcmd.exe INSTALL GENEAread
+IF EXIST C:\R2141\bin\x64\Rcmd.exe C:\R2141\bin\x64\Rcmd.exe INSTALL GENEAread
 pause
-IF EXIST C:\R\R-2.14.1\bin\x64\Rcmd.exe C:\R\R-2.14.1\bin\x64\Rcmd.exe INSTALL --build ReadGenea
-IF EXIST C:\R2141\bin\x64\Rcmd.exe C:\R2141\bin\x64\Rcmd.exe INSTALL --build ReadGenea
+IF EXIST C:\R\R-2.14.1\bin\x64\Rcmd.exe C:\R\R-2.14.1\bin\x64\Rcmd.exe INSTALL --build GENEAread
+IF EXIST C:\R2141\bin\x64\Rcmd.exe C:\R2141\bin\x64\Rcmd.exe INSTALL --build GENEAread
 pause
